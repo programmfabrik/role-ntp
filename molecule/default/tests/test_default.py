a1f71dd03e46a268f5fd1641ebd4bdc1720af475
Brian Wiborg <baccenfutter@c-base.org>
2017-09-18 11:31:04 +0200

    add molecule tests

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_package(File):
+    f = File('/usr/sbin/ntpd')
+    assert not f.exists
